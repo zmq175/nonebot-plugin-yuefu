bb4abe3e4e718d50b155dfccc677bf464b095141
Chengzhi <zmq175@qq.com>
2023-05-30 01:59:58 +0800

    url

--- nonebot_plugin_yuefu/__init__.py
+++ nonebot_plugin_yuefu/__init__.py
@@ -86,7 +86,7 @@ async def _(state: T_State, words: str = ArgStr("words")):
             'outputaudio.wav', open('outputaudio.wav', 'rb'),
             'audio/wav'))
     ]
-    url = "https://u9c50-6a4b59ba.neimeng.seetacloud.com:6443/voiceChangeModel"
+    url = "https://u99742-9438-6a636289.neimeng.seetacloud.com:6443/voiceChangeModel"
 
     payload = {'fPitchChange': '1',
                'sampleRate': '44100'}
